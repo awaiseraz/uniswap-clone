d9b017ff6b41838bbd13f1d0d22b8fcc2a20d4e0
Awais <m.awaisraz786@gmail.com>
2025-07-07 22:09:24 +0500

    start creating UniswapV2ERC20 contract

--- contracts/UniswapV2ERC20.sol
+++ contracts/UniswapV2ERC20.sol
@@ -1,9 +1,23 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.20;
 
-import "./interfaces/IUniswapV2ERC20.sol";
+//import "./interfaces/IUniswapV2ERC20.sol";
 
-abstract contract UniswapV2ERC20 is IUniswapV2ERC20 {
-    
+contract UniswapV2ERC20 {
+
+    string public constant name = "Uniswap V2";
+    string public constant symbol = "UNI-V2";
+    uint8 public constant decimals = 18;
+    uint public totalSupply;
+    mapping (address => uint) public balanceOf;
+    mapping (address => mapping(address => uint)) public allowance;
+
+    bytes32 public DOMAIN_SEPARATOR;
+
+    bytes32 public constant PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;mapping (address => uint) public nonces;
+
+    event Approval(address indexed owner, address indexed spender, uint value);
+    event Transfer(address indexed from, address indexed to, uint value);
+
 
 }
