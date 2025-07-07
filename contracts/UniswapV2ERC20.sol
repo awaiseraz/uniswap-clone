9a6c1b9371490496dc60bf184a4402e80b670f86
Awais <m.awaisraz786@gmail.com>
2025-07-07 21:38:07 +0500

    Adding IUniswapV2ERC20 interface

---
+++ contracts/UniswapV2ERC20.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
+
+import "./interfaces/IUniswapV2ERC20.sol";
+
+abstract contract UniswapV2ERC20 is IUniswapV2ERC20 {
+    
+
+}
